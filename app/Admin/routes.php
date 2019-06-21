a219a8c31d7450c18ffe15dee3b754a046205bb2
WIN_DAI <yamaguchi.dai31@gmail.com>
2019-06-22 00:13:17 +0900

    FIX:routeをリファクタリング

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,16 +5,19 @@ use Illuminate\Routing\Router;
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('admin.home');
 
-    $router->resource('master/customer',\MCustomerController::class);
+    Route::prefix('master')->group(function (Router $router) {
 
-    $router->resource('master/item',\MItemController::class);
-    $router->resource('master/tax',\MTaxController::class);
+        $router->resource('customer', \MCustomerController::class);
+        $router->resource('item', \MItemController::class);
+        $router->resource('tax', \MTaxController::class);
+
+    });
 
 });
