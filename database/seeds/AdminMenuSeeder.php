8ad2d0458d3c4b25233f96b291134e1b05a45fc7
WIN_DAI <yamaguchi.dai31@gmail.com>
2019-06-22 00:30:58 +0900

    ADD:メニュー追加

--- database/seeds/AdminMenuSeeder.php
+++ database/seeds/AdminMenuSeeder.php
@@ -15,11 +15,16 @@ class AdminMenuSeeder extends Seeder {
             //Admin デフォルト
 //            [1, 0, 1, "Dashboard", "fa-bar-chart", "/", NULL],
             [2, 0, 99, "Admin", "fa-tasks", NULL, NULL],
-            [3, 2, 3, "Users,fa-users", "auth/users", NULL],
+            [3, 2, 3, "Users","fa-users", "auth/users", NULL],
             [4, 2, 4, "Roles", "fa-user", "auth/roles", NULL],
             [5, 2, 5, "Permission", "fa-ban", "auth/permissions", NULL],
             [6, 2, 6, "Menu", "fa-bars", "auth/menu", NULL],
             [7, 2, 7, "Operation log", "fa-history", "auth/logs", NULL],
+
+            [8, 0, 1, "Master", "fa-tasks", NULL, NULL],
+            [9, 8, 1, "顧客マスタ", "fa-address-book", "master/customer", NULL],
+            [10, 8, 2, "商品マスタ", "fa-barcode", "master/item", NULL],
+            [11, 8, 3, "税率マスタ", "fa-credit-card", "master/tax", NULL],
 
 
         ];
