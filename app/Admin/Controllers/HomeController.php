891a2898c09117cbc7b49124e0e855c47b9d9c64
WIN_DAI <yamaguchi.dai31@gmail.com>
2019-06-24 23:52:31 +0900

    FIX:TOP画面変更

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -7,27 +7,35 @@ use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\Collapse;
+use Encore\Admin\Widgets\Table;
+use Encore\Admin\Widgets\Box;
 
-class HomeController extends Controller
-{
-    public function index(Content $content)
-    {
+class HomeController extends Controller {
+    public function index(Content $content) {
         return $content
-            ->title('Dashboard')
-            ->description('Description...')
-            ->row(Dashboard::title())
+            ->title('ダッシュボード')
             ->row(function (Row $row) {
-
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                    $box = new Box('業務');
+                    $box->content(new Table([], [
+                        ['<a><i class="fa fa-file"></i><span style="margin-left: 10px">見積書</span></a>'],
+                        ['<a><i class="fa fa-file"></i><span style="margin-left: 10px">納品書</span></a>'],
+                        ['<a><i class="fa fa-file"></i><span style="margin-left: 10px">請求書</span></a>'],
+                        ['<a><i class="fa fa-file"></i><span style="margin-left: 10px">払込書</span></a>'],
+                        ['<a><i class="fa fa-file"></i><span style="margin-left: 10px">領収書</span></a>'],
+                    ], ['table-striped']));
+                    $column->append($box);
                 });
 
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
+                    $box = new Box('マスター');
+                    $box->content(new Table([], [
+                        ['<a><i class="fa fa-address-book"></i><span style="margin-left: 10px">顧客マスタ</span></a>'],
+                        ['<a><i class="fa fa-barcode"></i><span style="margin-left: 10px">商品マスタ</span></a>'],
+                        ['<a><i class="fa fa-credit-card"></i><span style="margin-left: 10px">税率マスタ</span></a>'],
+                    ], ['table-striped']));
+                    $column->append($box);
                 });
             });
     }
